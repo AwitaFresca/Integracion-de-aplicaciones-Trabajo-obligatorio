a45d7af0a2b015ed0de48f4711b2c5d9da3f2ee5
AwitaFresca <71898131+AwitaFresca@users.noreply.github.com>
2023-07-07 04:16:59 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const Server = require('./models/server')
+const Server = require('./models/server') //requerimos el Servicio
 
+//Creamos el servicio
 const server = new Server();
 
+//Corremos el servicio y escuchamos en el puerto 3000
 server.listen();
